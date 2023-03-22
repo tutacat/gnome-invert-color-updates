cd5bc370c2be9a3ca2c74d229c89694dca0cccb8
Sebastian Lindau-Skands <slindauskands@gmail.com>
2023-03-22 10:06:37 +0100

    Updated to comply with gnome extension policies

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-rm true-color-window-inverter@lynet101.zip
+rm true-color-window-invert@lynet101.zip
 glib-compile-schemas schemas/
-zip true-color-window-inverter@lynet101.zip  * **/* -x build.sh
+zip true-color-window-invert@lynet101.zip  * **/* -x build.sh
