2c2961eba60d41268154a8b1be507a200930621e
tutacat <fhwbml@gmail.com>
2024-06-19 03:38:34 +0100

    Update README.md

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-rm true-color-window-invert@lynet101.zip
+rm true-color-window-invert@tutacat.github.io.shell-extension.zip
 glib-compile-schemas schemas/
-zip true-color-window-invert@lynet101.zip  * **/* -x build.sh
+if command -v gnome-extensions; then
+  gnome-extensions pack .
+else
+  zip true-color-window-invert@tutacat.github.io.zip  * **/* -x build.sh
+fi
